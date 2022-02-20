4f28fe47733942a1c1ee2d8fdb2236f514856f62
Ogasa-Kazuma <knct0831@gmail.com>
2022-02-20 23:15:55 +0900

    temp: temp comp

--- PollutionCreater.py
+++ PollutionCreater.py
@@ -5,43 +5,38 @@ class PollutionCreater:
     def __init__(self):
         pass
 
-    def SphericalPollution(self, radius, pollution_min, pollution_max):
+    def SphericalPollution(self, diameter, pollution_at_center_point):
 
-        ##radiusが偶数や小数のときは受け付けないようにすべき？
-        pollution_decreasing_ratio = self.__Calc_Decreasing_Per_Distance(radius, pollution_min, pollution_max)
-        #汚染源の中心座標を計算
-        centerPoint = self.__DecideCenterPoint(radius)
-        #中心座標から離れるほど濃度を低く計算
-        self.__CalcPollutionsCorrespondingTo_xyzPoints(centerPoint, radius, pollution_decreasing_ratio)
-
-
-        return sphericalPollution
-
-    def __CalcPollutionsCorrespondingTo_xyzPoints(centerPoint, radius, pollution_decreasing_ratio):
+        ##diameterが奇数や小数のときは受け付けないようにすべき？
 
 
         sphericalPollution = list()
 
-        for x_i in range(0, radius + 1):
-            for y_i in range(0, radius + 1):
-                for z_i in range(0, radius + 1):
-                    self.__CalcDistanceFromCenter()
-                    sphericalPollution[x_i][y_i][z_i] = self.__CalcPollutionAtOnePoint()
+        for x_i in range(0, diameter + 1):
+            for y_i in range(0, diameter + 1):
+                for z_i in range(0, diameter + 1):
+                    pollutionPoint = Point(x_i, y_i, z_i)
+                    distance = self.__CalcDistanceFromCenter(pollutionPoint, diameter)
+                    sphericalPollution[x_i][y_i][z_i] = self.__CalcPollutionAtOnePoint(distance, pollution_at_center_point)
+
+        sphericalPollution = Pollution(sphericalPollution)
+        return sphericalPollution
 
 
+    def __CalcDistanceFromCenter(self, pollutionPoint, diameter):
 
-    def __CalcDistanceFromCenter(self, radius, x, y, z):
-        for x_count in range(0, radius + 1):
-            for y_count in range(0, radius + 1):
-                for z_count in range(0, radius + 1):
-                        pollutionPoint = Point(x_i, y_i, z_i)
-                        distance = pollutionPoint.distance(centerPoint)
+        centerPoint = Point(x = (diameter / 2), y = (diameter / 2), z = (diameter / 2))
+
+        distance = pollutionPoint.distance(centerPoint)
 
         return distance
 
 
 
-    def __Calc_Decreasing_Per_Distance(self, radius, pollution_min, pollution_max):
+    def __Calc_Decreasing_Per_Distance(self, diameter, pollution_min, pollution_max):
         pollution_range = pollution_max - pollution_min
-        decreasing_per_distance = pollution_range / radius
+        decreasing_per_distance = pollution_range / diameter
         return decreasing_per_distance
+
+    def __CalcPollutionAtOnePoint(self, distance):
+        pollution_decreasing_ratio = self.__Calc_Decreasing_Per_Distance(diameter, pollution_min, pollution_max)
